546ff3a375dd9c549f37fd9324098a8a949fcd4b
Benjamin Bannier <benjamin.bannier@corelight.com>
2024-08-30 14:49:59 +0200

    fixup! Add infrastructure to collect dataflow.

--- hilti/toolchain/src/compiler/cfg.cc
+++ hilti/toolchain/src/compiler/cfg.cc
@@ -18,6 +18,7 @@
 #include <hilti/ast/node.h>
 #include <hilti/ast/statement.h>
 #include <hilti/ast/statements/block.h>
+#include <hilti/ast/statements/continue.h>
 #include <hilti/ast/statements/declaration.h>
 #include <hilti/ast/statements/expression.h>
 #include <hilti/ast/statements/if.h>
