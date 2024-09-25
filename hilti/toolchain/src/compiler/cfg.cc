c7bb655418ffa82a1695ae3da3a8a6b6847ee09d
Benjamin Bannier <benjamin.bannier@corelight.com>
2024-09-25 12:00:13 +0200

    fixup! 7b6743206c2d2d80435acb57f8ac75c1ff1248b4

--- hilti/toolchain/src/compiler/cfg.cc
+++ hilti/toolchain/src/compiler/cfg.cc
@@ -535,7 +535,7 @@ struct DataflowVisitor : visitor::PreOrder {
 
 void CFG::populate_dataflow() {
     auto visit_node = [](const CXXGraph::Node<N>* n) -> Transfer {
-        if ( auto x = n->getData()->tryAs<MetaNode>() )
+        if ( n->getData()->isA<MetaNode>() )
             return {};
 
         auto v = DataflowVisitor(n);
