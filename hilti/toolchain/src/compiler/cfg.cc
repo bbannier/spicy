54f3b9b91bba05c82b1cbab15c5a07219b772c90
Benjamin Bannier <benjamin.bannier@corelight.com>
2024-10-11 16:02:09 +0200

    fixup! fixup! fixup! fixup! Mark updates to `inout` function parameters as use.

--- hilti/toolchain/src/compiler/cfg.cc
+++ hilti/toolchain/src/compiler/cfg.cc
@@ -826,6 +826,7 @@ std::vector<const CXXGraph::Node<CFG::N>*> CFG::unreachable_statements() const {
                 for ( auto&& [n_, _] : dataflow.at(in).gen ) {
                     if ( n_->isA<declaration::GlobalVariable>() )
                         ++uses[in];
+
                     else if ( auto&& p = n_->tryAs<declaration::Parameter>();
                               p && p->kind() == parameter::Kind::InOut ) {
                         ++uses[in];
